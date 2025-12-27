31655f272349df8ed46b86e5f098e90479458f7f
Kerlon Ribeiro <124305955+kerlonr@users.noreply.github.com>
2025-12-27 18:55:11 -0300

    Install Python dependencies in a virtual environment
    Updated Python dependency installation to use virtual environment.

--- initScript.sh
+++ initScript.sh
@@ -37,8 +37,11 @@ fi
 
 # Dependências Python
 echo "[4/5] Instalando dependências Python..."
-pip install --upgrade pip
-pip install -r requirements.txt
+
+python3 -m venv venv
+source venv/bin/activate
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
 # Bluetooth
 echo "[5/5] Habilitando Bluetooth... (Futuro)"
